ac6c42659e9511a210c98ddde0403a9f04de59c3
grg121 <navarromoralesdev@gmail.com>
2020-11-12 18:44:02 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import setuptools
+
+try:
+    with open('README.md', 'r', encoding='utf-8') as fh:
+        long_description = fh.read()
+except (IOError, OSError):
+    long_description = ''
+
+setuptools.setup(
+    name='xontrib-syslog-shell-profiler',
+    version='0.1.0',
+    license='MIT',
+    author='Francisco Navarro',
+    author_email='navarromoralesdev@gmail.com',
+    description="A Xonsh plugin to profile and log command execution to a syslog file.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    python_requires='>=3.6',
+    install_requires=['xonsh'],
+    packages=['xontrib'],
+    package_dir={'xontrib': 'xontrib'},
+    package_data={'xontrib': ['*.xsh']},
+    platforms='any',
+    url='https://github.com/grg121/xontrib-syslog-shell-profiler',
+    project_urls={
+        "Documentation": "https://github.com/grg121/xontrib-syslog-shell-profiler/blob/master/README.md",
+        "Code": "https://github.com/grg121/xontrib-syslog-shell-profiler",
+        "Issue tracker": "https://github.com/grg121/xontrib-syslog-shell-profiler/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: System :: Shells",
+        "Topic :: System :: System Shells",
+        "Topic :: Terminals",
+    ]
+)
---
+++ xontrib/syslog-shell-profiler.xsh
@@ -0,0 +1,8 @@
+import xonsh
+
+
+__all__ = ()
+
+print('This is syslog-shell-profiler!')
+
+# Good start! Get more documentation -> https://xon.sh/contents.html#guides
